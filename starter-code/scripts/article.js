d77e605c53b87e1b17ec80dc2fb908fc0ebd23b3
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-02 12:20:49 -0700

    In class lab work

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -21,15 +21,16 @@ Article.prototype.toHtml = function() {
 
   return template(this);
 };
+function populateArticles() {
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
 
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+  rawData.forEach(function(articleObject) {
+    articles.push(new Article(articleObject));
+  })
 
-rawData.forEach(function(articleObject) {
-  articles.push(new Article(articleObject));
-})
-
-articles.forEach(function(article){
-  $('#articles').append(article.toHtml())
-});
+  articles.forEach(function(article){
+    $('#articles').append(article.toHtml())
+  });
+}
